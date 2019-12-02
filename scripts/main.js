c9d5d74d97208bd0a583c67b1f72a1b69c42c2d1
alsubi <alsubaie22145@gmail.com>
2019-12-02 11:55:57 +0300

    create an html page about Tic Tac Toe. simple design in css ,9 boxes. in JS : turn based player . logic to win.

---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+let array = ['', '', '', '', '', '','', '', ''];
+function win() {
+    
+    if (array[0] == array[1] && array[0] == array[2] && Boolean(array[0])==true) {
+        alert("the winner is " + array[0]);
+    }else if(array[3] == array[4] && array[3] == array[5] && Boolean(array[3])==true) {
+        alert("the winner is " + array[3]);
+    }else if(array[6] == array[7] && array[6] == array[8] && Boolean(array[6])==true) {
+        alert("the winner is " + array[6]);
+    }else if(array[0] == array[3] && array[0] == array[6] && Boolean(array[0])==true) {
+        alert("the winner is " + array[0]);
+    }else if(array[1] == array[4] && array[1] == array[7] && Boolean(array[1])==true) {
+        alert("the winner is " + array[1]);
+    }else if(array[2] == array[5] && array[2] == array[8] && Boolean(array[2])==true) {
+        alert("the winner is " + array[2]);
+    }else if(array[6] == array[4] && array[6] == array[2] && Boolean(array[6])==true) {
+        alert("the winner is " + array[6]);
+    }else if(array[0] == array[4] && array[0] == array[8] && Boolean(array[0])==true) {
+        alert("the winner is " + array[0]);
+    }else if(array[6] == array[4] && array[6] == array[2] && Boolean(array[6])==true) {
+        alert("the winner is " + array[6]);
+    }
+
+
+}
+$(function () {
+
+
+
+    // let array = ['', '', '', '', '', '','', '', ''];
+
+
+
+    
+
+    let counter = 0;
+    $(".container div").click(function (event) {
+
+
+
+        if (counter % 2 === 0) {
+            $(event.target).text("x");
+            $(event.target).off();
+            let value = event.target.id;
+
+
+            array[value] = "x";
+            win()
+        } else if (counter % 2 !== 0) {
+            $(event.target).text("o");
+            $(event.target).off();
+            let value = event.target.id;
+            array[value] = "o";
+            win()
+        }
+        counter += 1;
+        console.log(array)
+
+    })
+
+
+
+
+
+
+})
