8edc1b3f620177c785ff0bdbaef0d7624a706ed6
alsubi <alsubaie22145@gmail.com>
2019-12-04 22:49:05 +0300

    sound affect animation

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
-# Project : Tic Tac Toe 
+# Project : Tic Tac Toe :
 
-   The project is a game of Tic Tac Toe with 2 players and show the winner in game .
+    A game of Tic Tac Toe with 2 players plays in the same device .
 
-## My Project Deployment
+## My Project Deployment:
+https://alsubi.github.io/project-1/
 
-## I created Project by use
+## I created Project by use :
 * HTML
 * CSS
     * responsive with mobile versions
 * JS
 * jQuery
+* sounds affect
 
 
+---
 
+## lines in " js " :
 
-### Describe lines in the code " js "
-### array 
-```js
-```
-#### function reset()
+* ### function reset()
+To re-rounds the game .
+ Reset game  .
+
 ```js
 function reset(){
     array = ['', '', '', '', '', '','', '', ''];
@@ -31,7 +34,11 @@ function reset(){
         $(".container div").css("background","rgb(0,0,0");
 }
 ```
-#### function win()
+
+* ### function win()
+
+compare between element in array to know who win in the game
+
 
 ```js
     if (array[0] == array[1] && array[0] == array[2] && Boolean(array[0])==true) {
@@ -41,8 +48,10 @@ function reset(){
          $(".container #0,#1,#2").css("background","rgba(0,0,0,0.7");
  
 ```
-#### function game() 
 
+* ### function game() 
+switch turns between X and O .
+- by useing conditiono  (even and odd numbers) to switch in every event that occurs
 ```js  
 function game(event) {
      
@@ -51,7 +60,6 @@ function game(event) {
         $(event.target).off();
         $(".playerTurn").text("o")
         let value = event.target.id;    
-       
         array[value] = "x";
         win()
     } else if (counter % 2 !== 0) {
@@ -67,3 +75,20 @@ function game(event) {
 
 }
 ```
+---
+
+## Audio and image source :
+* [button sound](http://soundbible.com/772-Button.html)
+* [Fireworks sound](http://soundbible.com/712-Fireworks-And-Crowd.html)
+* [tic tac toe image](https://www.seekclipart.com/clipng/big/128-1285954_tick-tack-toe-clipart-clipground-pot-of-gumbo.png)
+
+
+## Challenges:
+How to design and control the elements in CSS
+
+
+## Unsolved problems which would be fixed in future iterations:
+* play with pc 
+* show win times for each player
+* let player choose (O or X) to play 
+ 
--- scripts/main.js
+++ scripts/main.js
@@ -1,85 +1,90 @@
-let array = ['', '', '', '', '', '','', '', ''];
-let counter = 0;  //مصفوفه تخزن قيمة ما تم تحديده في الحدث
+const clickSound = new Audio("scripts/souc.mp3")
+const winSound = new Audio("scripts/Fireworks.mp3")
+
+let array = ['', '', '', '', '', '', '', '', ''];
+let counter = 0;
 $(".msg>.player,.winner").hide();
-function reset(){
-    array = ['', '', '', '', '', '','', '', ''];
+
+function reset() {
+    array = ['', '', '', '', '', '', '', '', ''];
     $(".container div").text("");
-        $(".container div").off();
-        $(".container div").on("click", game)
-        counter = 0;
-        $(".msg>.player,.winner").hide();
-        $(".playerTurn").text("");
-        $(".container div").css("background","rgb(0,0,0");
+    $(".container div").off();
+    $(".container div").on("click", game)
+    counter = 0;
+    $(".msg>.player,.winner").hide();
+    $(".playerTurn").text("");
+    $(".container div").css("background", "rgb(0,0,0");
 }
-function playValue(){
+
+function playValue() {
     $(".playerTurn").text("")
     $(".container div").off();
     $(".msg>.player,.winner").show();
-
+    winSound.play()
 }
 
-function win() {  //checking winner   
-    
-    if (array[0] == array[1] && array[0] == array[2] && Boolean(array[0])==true) {
-         
-         $(".player").text(array[0]);
-         playValue()
-         $(".container #0,#1,#2").css("background","rgba(0,0,0,0.7");
-    }else if(array[3] == array[4] && array[3] == array[5] && Boolean(array[3])==true) {
-        
-        $(".player").text(array[3]);
-        playValue()
-        $(".container #3,#4,#5").css("background","rgba(0,0,0,0.7")
-    }else if(array[6] == array[7] && array[6] == array[8] && Boolean(array[6])==true) {
-        
-        $(".player").text(array[6]);
-        playValue()
-        $(".container #6,#7,#8").css("background","rgba(0,0,0,0.7")
-    }else if(array[0] == array[3] && array[0] == array[6] && Boolean(array[0])==true) {
-       
+
+function win() {
+
+    if (array[0] == array[1] && array[0] == array[2] && Boolean(array[0]) == true) {
+
         $(".player").text(array[0]);
         playValue()
-        $(".container #0,#3,#6").css("background","rgba(0,0,0,0.7")
-    }else if(array[1] == array[4] && array[1] == array[7] && Boolean(array[1])==true) {
-        $(".player").text(array[1]);
+        $(".container #0,#1,#2").css("background", "rgba(0,0,0,0.7");
+    } else if (array[3] == array[4] && array[3] == array[5] && Boolean(array[3]) == true) {
+
+        $(".player").text(array[3]);
         playValue()
-        $(".container #1,#4,#7").css("background","rgba(0,0,0,0.7")
-    }else if(array[2] == array[5] && array[2] == array[8] && Boolean(array[2])==true) {
-        $(".player").text(array[2]);
-        playValue()
-        $(".container #2,#5,#8").css("background","rgba(0,0,0,0.7")
-    }else if(array[6] == array[4] && array[6] == array[2] && Boolean(array[6])==true) {
+        $(".container #3,#4,#5").css("background", "rgba(0,0,0,0.7")
+    } else if (array[6] == array[7] && array[6] == array[8] && Boolean(array[6]) == true) {
+
         $(".player").text(array[6]);
         playValue()
-        $(".container #6,#4,#2").css("background","rgba(0,0,0,0.7")
-    }else if(array[0] == array[4] && array[0] == array[8] && Boolean(array[0])==true) {
+        $(".container #6,#7,#8").css("background", "rgba(0,0,0,0.7")
+    } else if (array[0] == array[3] && array[0] == array[6] && Boolean(array[0]) == true) {
+
+        $(".player").text(array[0]);
+        playValue()
+        $(".container #0,#3,#6").css("background", "rgba(0,0,0,0.7")
+    } else if (array[1] == array[4] && array[1] == array[7] && Boolean(array[1]) == true) {
+        $(".player").text(array[1]);
+        playValue()
+        $(".container #1,#4,#7").css("background", "rgba(0,0,0,0.7")
+    } else if (array[2] == array[5] && array[2] == array[8] && Boolean(array[2]) == true) {
+        $(".player").text(array[2]);
+        playValue()
+        $(".container #2,#5,#8").css("background", "rgba(0,0,0,0.7")
+    } else if (array[6] == array[4] && array[6] == array[2] && Boolean(array[6]) == true) {
+        $(".player").text(array[6]);
+        playValue()
+        $(".container #6,#4,#2").css("background", "rgba(0,0,0,0.7")
+    } else if (array[0] == array[4] && array[0] == array[8] && Boolean(array[0]) == true) {
         $(".player").text(array[0]);
         $(".container div").off()
         playValue()
-        $(".container #0,#4,#8").css("background","rgba(0,0,0,0.7")
-    }else if(array[6] == array[4] && array[6] == array[2] && Boolean(array[6])==true) {
+        $(".container #0,#4,#8").css("background", "rgba(0,0,0,0.7")
+    } else if (array[6] == array[4] && array[6] == array[2] && Boolean(array[6]) == true) {
         $(".player").text(array[6]);
         playValue()
-        $(".container #6,#4,#2").css("background","rgba(0,0,0,0.7")
-        
-    }
-    else if (counter === 8) {
+        $(".container #6,#4,#2").css("background", "rgba(0,0,0,0.7")
+
+    } else if (counter === 8) {
         $(".playerTurn").text("draw")
     }
-    
+
 
 
 }
-    
+
 function game(event) {
-     
-         if (counter % 2 === 0) {
+    clickSound.play()
+    if (counter % 2 === 0) {
         $(event.target).text("x");
         $(event.target).off();
         $(".playerTurn").text("o")
-        let value = event.target.id; // value / الذي تم تحديده في الحدث / للمتغير /(id)/ اعطاء قيمة   
-        //id is number in html child of ".container" from 0 to 8 as array index that will save text 
-        array[value] = "x";// index / في المصفوفه بحسب   /"x"/ تخزين
+        let value = event.target.id;
+
+        array[value] = "x";
         win()
     } else if (counter % 2 !== 0) {
         $(event.target).text("o");
@@ -89,15 +94,13 @@ function game(event) {
         array[value] = "o";
         win()
     }
-    
 
-    counter += 1; //click counter for if-condetion 
-    //console.log(counter)
-  
+
+    counter += 1;
+
+
 
 }
 
 $(".container div").click(game)
 $(".reset").click(reset)
-
-
